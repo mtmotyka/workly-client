6338d949d1084dc8f990cd073ea1f0cf93baeaec
mikovsky <dudek.michal1996@gmail.com>
2021-01-18 21:58:44 +0100

    Routes component added, gitignore updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Layout from "./components/Layout/Layout";
+import Routes from "./routes";
 
 const App = () => {
   return (
     <div>
-      <Layout />
+      <Routes />
     </div>
   );
 };
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import LoginPage from "../LoginPage/LoginPage";
+import Sidebar from "../Sidebar/Sidebar";
 
 import "./layout.scss";
-import Sidebar from "../Sidebar/Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <main className="layout">
-      <BrowserRouter>
-        <Route path="/login" exact component={LoginPage} />
-      </BrowserRouter>
       <Sidebar />
       <section className="layout__content">{children}</section>
     </main>
