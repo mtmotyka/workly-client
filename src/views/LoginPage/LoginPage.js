3895b3a85d610445792e220e0550581bc782f601
Mateusz Motyka <m.motyka@zensite.pl>
2021-02-21 22:21:00 +0100

    fix https error

--- src/views/LoginPage/LoginPage.js
+++ src/views/LoginPage/LoginPage.js
@@ -17,7 +17,7 @@ const LoginPage = (props) => {
     e.preventDefault();
 
     const response = await axios.post(
-      "http://workly.mikovsky-cloud.com/api/auth/login",
+      "https://cors-anywhere.herokuapp.com/http://workly.mikovsky-cloud.com/api/auth/login",
       {
         email: email,
         password: password,
