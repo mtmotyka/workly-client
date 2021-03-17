d20c245350e2307c5941d6d4f9209205edb62937
Mateusz Motyka <m.motyka@zensite.pl>
2021-03-17 16:00:07 +0100

    new api url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://mikovsky-cloud.com/workly";
+export const BACKEND_URL = "https://mikovskycloud.com/workly";
 export const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,64}$/; //8-64 signs, at least one number, one lowercase, one uppercase, no spaces
 export const emailValidation = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
